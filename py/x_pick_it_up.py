b9dd52f72f203f9d1f266659edeb21e0dfa2d69c
terrificmn <mildsm@gmail.com>
2022-10-20 19:17:14 +0900

    py scripts added for xArm, movements improvements

---
+++ py/x_pick_it_up.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2022, UFACTORY, Inc.
+# All rights reserved.
+#
+# Author: Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
+
+"""
+# Notice
+#   1. Changes to this file on Studio will not be preserved
+#   2. The next conversion will overwrite the file with the same name
+# 
+# xArm-Python-SDK: https://github.com/xArm-Developer/xArm-Python-SDK
+#   1. git clone git@github.com:xArm-Developer/xArm-Python-SDK.git
+#   2. cd xArm-Python-SDK
+#   3. python setup.py install
+"""
+import sys
+import math
+import time
+import queue
+import datetime
+import random
+import traceback
+import threading
+from xarm import version
+from xarm.wrapper import XArmAPI
+
+
+class RobotMain(object):
+    """Robot Main Class"""
+    def __init__(self, robot, **kwargs):
+        self.alive = True
+        self._arm = robot
+        self._tcp_speed = 100
+        self._tcp_acc = 2000
+        self._angle_speed = 20
+        self._angle_acc = 500
+        self._variables = {}
+        self._robot_init()
+
+    # Robot init
+    def _robot_init(self):
+        self._arm.clean_warn()
+        self._arm.clean_error()
+        self._arm.motion_enable(True)
+        self._arm.set_mode(0)
+        self._arm.set_state(0)
+        time.sleep(1)
+        self._arm.register_error_warn_changed_callback(self._error_warn_changed_callback)
+        self._arm.register_state_changed_callback(self._state_changed_callback)
+        if hasattr(self._arm, 'register_count_changed_callback'):
+            self._arm.register_count_changed_callback(self._count_changed_callback)
+
+    # Register error/warn changed callback
+    def _error_warn_changed_callback(self, data):
+        if data and data['error_code'] != 0:
+            self.alive = False
+            self.pprint('err={}, quit'.format(data['error_code']))
+            self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+
+    # Register state changed callback
+    def _state_changed_callback(self, data):
+        if data and data['state'] == 4:
+            self.alive = False
+            self.pprint('state=4, quit')
+            self._arm.release_state_changed_callback(self._state_changed_callback)
+
+    # Register count changed callback
+    def _count_changed_callback(self, data):
+        if self.is_alive:
+            self.pprint('counter val: {}'.format(data['count']))
+
+    def _check_code(self, code, label):
+        if not self.is_alive or code != 0:
+            self.alive = False
+            ret1 = self._arm.get_state()
+            ret2 = self._arm.get_err_warn_code()
+            self.pprint('{}, code={}, connected={}, state={}, error={}, ret1={}. ret2={}'.format(label, code, self._arm.connected, self._arm.state, self._arm.error_code, ret1, ret2))
+        return self.is_alive
+
+    @staticmethod
+    def pprint(*args, **kwargs):
+        try:
+            stack_tuple = traceback.extract_stack(limit=2)[0]
+            print('[{}][{}] {}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), stack_tuple[1], ' '.join(map(str, args))))
+        except:
+            print(*args, **kwargs)
+
+    @property
+    def is_alive(self):
+        if self.alive and self._arm.connected and self._arm.error_code == 0:
+            if self._arm.state == 5:
+                cnt = 0
+                while self._arm.state == 5 and cnt < 5:
+                    cnt += 1
+                    time.sleep(0.1)
+            return self._arm.state < 4
+        else:
+            return False
+
+    # Robot Main Run
+    def run(self):
+        try:
+            # Joint Motion
+            self._angle_speed = 20
+            self._angle_acc = 400
+            for i in range(int(1)):
+                if not self.is_alive:
+                    break
+                code = self._arm.set_servo_angle(angle=[-5.8, -40.6, -7.1, 41.4, 176.5, -19.7, -0.6], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_servo_angle(angle=[17.7, -11.6, -4.6, 58.1, 176.8, 22.1, 0.7], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_servo_angle(angle=[10.6, 7.1, -3.3, 58.7, 196.5, 38.2, -11.1], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_servo_angle(angle=[10.6, 18.9, -4.7, 71.3, 196.5, 38.2, -11.1], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_gripper_position(850, wait=True, speed=5000, auto_enable=True)
+                if not self._check_code(code, 'set_gripper_position'):
+                    return
+                code = self._arm.set_gripper_position(458, wait=True, speed=5000, auto_enable=True)
+                if not self._check_code(code, 'set_gripper_position'):
+                    return
+                code = self._arm.set_servo_angle(angle=[10.6, 0.1, -5.8, 74.6, 196.5, 13.3, -15.6], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_servo_angle(angle=[-0.7, -73.0, -3.5, 69.2, 178.8, -54.4, -6.5], speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1.0)
+                if not self._check_code(code, 'set_servo_angle'):
+                    return
+                code = self._arm.set_pause_time(2)
+                if not self._check_code(code, 'set_pause_time'):
+                    return
+        except Exception as e:
+            self.pprint('MainException: {}'.format(e))
+        self.alive = False
+        self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+        self._arm.release_state_changed_callback(self._state_changed_callback)
+        if hasattr(self._arm, 'release_count_changed_callback'):
+            self._arm.release_count_changed_callback(self._count_changed_callback)
+
+
+if __name__ == '__main__':
+    RobotMain.pprint('xArm-Python-SDK Version:{}'.format(version.__version__))
+    arm = XArmAPI('192.168.1.204', baud_checkset=False)
+    robot_main = RobotMain(arm)
+    robot_main.run()
--- src/drive.cpp
+++ src/drive.cpp
@@ -3,6 +3,7 @@
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/LaserScan.h>
 #include <tf/tf.h>
+#include <ros/package.h>
 
 class DriveNPick {
 private:
@@ -15,6 +16,7 @@ private:
     ros::Time current_time;
     nav_msgs::Odometry odom_msg;
     sensor_msgs::LaserScan scan_msg;
+    double scan_front = 100.0; // init
 
     double yaw;
 
@@ -29,9 +31,9 @@ public:
         spinner.start();
 
         pub_cmd = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-        sub_odom = nh.subscribe<nav_msgs::Odometry>("/odom", 10, &DriveNPick::odomCb, this);
+        sub_odom = nh.subscribe<nav_msgs::Odometry>("/md_odom", 10, &DriveNPick::odomCb, this);
         sub_scan = nh.subscribe<sensor_msgs::LaserScan>("/scan", 10, &DriveNPick::scanCb, this);
-        this->drive();
+        this->drive();   
     }
 
     ~DriveNPick() {}
@@ -46,6 +48,7 @@ public:
         double distance = 0.0;
         ros::Duration duration_time;
         ros::Duration(0.3).sleep(); // prevent from saving 0 to start_time
+        std::string pkgpath = ros::package::getPath("drive_n_pick");
         this->setStartTime();
         
         ros::Rate lr(10);
@@ -53,53 +56,153 @@ public:
         while(ros::ok()) {
             geometry_msgs::Twist cmd;
             if(this->sequence == 0) {
-                cmd.linear.x = 0.3;
+                cmd.linear.x = 0.4;
+                cmd.angular.z = 0.0;  //0.4
                 distance = cmd.linear.x * duration_time.toSec();
 
-                ROS_INFO("distance: %lf", distance);
-                ROS_INFO("total time: %lf", duration_time.toSec());
+                // ROS_INFO("distance: %lf", distance);
+                // ROS_INFO("total time: %lf", duration_time.toSec());
                 
-                if(distance > 1.5) {
+                if(distance > 1.54 || (this->scan_front < 0.2)) {   /// distance 2.35
                     ROS_INFO("robot stopped");
                     this->sequence = 1;
                     distance = 0.0; //reset
                     cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
                     this->setStartTime();
                 }
 
             } else if(this->sequence == 1) {
                 cmd.linear.x = 0.0;
-                cmd.angular.z = 0.3;
-                ROS_INFO("robot rotate");
+                cmd.angular.z = 0.5; // 0.0
+                distance = cmd.linear.x * duration_time.toSec();
+
+                // ROS_INFO("distance: %lf", distance);
+                // ROS_INFO("total time: %lf", duration_time.toSec());
+                ROS_INFO("yaw: %lf", this->yaw);
+                
+                if(this->yaw > 1.51) {
+                    ROS_INFO("robot stopped");
+                    this->sequence = 2;
+                    distance = 0.0; //reset
+                    cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
+                    this->setStartTime();
+                }
+
+            } else if(this->sequence == 2) {
+                cmd.linear.x = 0.3;
+                cmd.angular.z = 0.0; // 0.0
+                distance = cmd.linear.x * duration_time.toSec();
+
+                // ROS_INFO("distance: %lf", distance);
+                // ROS_INFO("total time: %lf", duration_time.toSec());
+                
+                if(distance > 1.91 || (this->scan_front < 0.18)) {
+                    ROS_INFO("robot stopped");
+                    this->sequence = 3;
+                    distance = 0.0; //reset
+                    cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
+                    this->setStartTime();
+                }
+
+            }
+             ///// waiting 
+             else if(this->sequence == 3) {
+                cmd.linear.x = 0.0;
+                cmd.angular.z = 0.0;
+                ROS_INFO("xArm execute");
                 // ROS_INFO("total time: %lf", duration_time.toSec());
 
                 ROS_INFO("yaw : %lf", this->yaw);
 
-                if(duration_time.toSec() > 4.0) {
+                if(duration_time.toSec() > 2.0) {
+                    /// xarm execute
+                    std::string py_cmd = "python3 " + pkgpath + "/py/x_pick_it_up.py";
+                    system(py_cmd.c_str());
                     cmd.angular.z = 0.0;
-                    this->sequence = 2;
+                    distance = 0.0; //reset
+                    this->sequence++;
                     this->setStartTime();
-                    ROS_INFO("robot rotation stopped");
+                    ROS_WARN("xarm now executed");
+                }
+            
+            }  else if(this->sequence == 4) {
+                cmd.linear.x = 0.0;
+                cmd.angular.z = -0.6;
+                ROS_INFO("robot rotate and moving");
+
+                distance = cmd.linear.x * duration_time.toSec();
+
+                ROS_INFO("yaw: %lf", this->yaw);
+                if(this->yaw < 0.27) {
+                    ROS_INFO("robot stopped");
+                    this->sequence++;
+                    distance = 0.0; //reset
+                    cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
+                    this->current_time = ros::Time::now();
+                    this->setStartTime();
+                }
+            } 
+            else if(this->sequence == 5) {
+                cmd.linear.x = 0.3;
+                cmd.angular.z = 0.0;
+                ROS_INFO("robot moving");
+
+                distance = cmd.linear.x * duration_time.toSec();
+
+                ROS_INFO("distance: %lf", distance);
+
+                if(distance > 1.0 || (this->scan_front < 0.2)) {
+                    ROS_INFO("robot stopped");
+                    this->sequence++;
+                    distance = 0.0; //reset
+                    cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
+                    this->setStartTime();
                 }
 
-            }  else if(this->sequence == 2) {
-                cmd.linear.x = 0.1;
-                cmd.angular.z = 0.3;
+            } else if(this->sequence == 6) {
+                cmd.linear.x = 0.0;
+                cmd.angular.z = -0.6;
+                ROS_INFO("robot rotate");
+
+                // ROS_INFO("distance: %lf", distance);
+                ROS_INFO("yaw: %lf", this->yaw);
+
+                if(this->yaw < -1.51 || (this->scan_front < 0.2)) {
+                    ROS_INFO("robot stopped");
+                    this->sequence++;
+                    distance = 0.0; //reset
+                    cmd.linear.x = 0.0;
+                    cmd.angular.z = 0.0;
+                    this->setStartTime();
+                }
+            } 
+             else if(this->sequence == 7) {
+                cmd.linear.x = 0.3;
+                cmd.angular.z = 0.0;
                 ROS_INFO("robot rotate and moving");
 
                 distance = cmd.linear.x * duration_time.toSec();
 
                 ROS_INFO("distance: %lf", distance);
 
-                if(distance > 1.0) {
+                if(distance > 1.6 || (this->scan_front < 1.0)) {
                     ROS_INFO("robot stopped");
-                    this->sequence = 3;
+                    this->sequence++;
                     distance = 0.0; //reset
                     cmd.linear.x = 0.0;
-                    this->current_time = ros::Time::now();
+                    cmd.angular.z = 0.0;
                     this->setStartTime();
                 }
 
+            }  else if(this->sequence == 8) {
+                std::string py_cmd = "python3 " + pkgpath + "/py/x_put_it_down.py";
+                system(py_cmd.c_str());  ///blocking
+                this->sequence = 10 ;
             }
 
             this->end_time = ros::Time().now();
@@ -115,7 +218,7 @@ public:
         this->odom_msg.pose.pose.orientation.y = msg->pose.pose.orientation.y;
         this->odom_msg.pose.pose.orientation.z = msg->pose.pose.orientation.z;
         this->odom_msg.pose.pose.orientation.w = msg->pose.pose.orientation.w;
-        // this->yaw = this->quaternionToYaw();
+        this->yaw = this->quaternionToYaw();
     }
 
     double quaternionToYaw() {
@@ -135,8 +238,10 @@ public:
     }
 
     void scanCb(const sensor_msgs::LaserScan::ConstPtr &msg) {
-        ROS_INFO("Scan %f", msg->ranges[0]);
-        // this->scan_msg.ranges[0] = msg->ranges[0];
+        // ROS_INFO("Scan %f", msg->ranges[365]);  //360  // almost front 
+        // this->scan_front = msg->ranges[365];
+        this->scan_front = (msg->ranges[361]+msg->ranges[362]+msg->ranges[363]+msg->ranges[364]+msg->ranges[365]) /5;
+        // ROS_INFO("Scan %f", this->scan_front); 
     }
 
 };
