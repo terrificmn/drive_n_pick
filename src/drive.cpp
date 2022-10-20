631b13614d498318e7c9258bbf7af26c63f8ed51
terrificmn <mildsm@gmail.com>
2022-10-20 10:54:09 +0900

    odom, scan added / test

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,14 +1,23 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <tf/tf.h>
 
 class DriveNPick {
 private:
     ros::NodeHandle nh;
     ros::Publisher pub_cmd;
+    ros::Subscriber sub_odom;
+    ros::Subscriber sub_scan;
     ros::AsyncSpinner spinner;
     int sequence = 0;
     ros::Time current_time;
+    nav_msgs::Odometry odom_msg;
+    sensor_msgs::LaserScan scan_msg;
+
+    double yaw;
+
 
 protected:
     ros::Time start_time, end_time;
@@ -20,6 +29,8 @@ public:
         spinner.start();
 
         pub_cmd = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+        sub_odom = nh.subscribe<nav_msgs::Odometry>("/odom", 10, &DriveNPick::odomCb, this);
+        sub_scan = nh.subscribe<sensor_msgs::LaserScan>("/scan", 10, &DriveNPick::scanCb, this);
         this->drive();
     }
 
@@ -27,15 +38,15 @@ public:
 
     void setStartTime() {
         this->start_time = ros::Time().now();
+        ROS_INFO("start time set!");
+        // ROS_WARN("starttime: %lf", this->start_time.toSec());
     }
 
     void drive() {
-        double store_linear_x = 0.0;
         double distance = 0.0;
         ros::Duration duration_time;
-        ros::Duration sec_duration_time;
+        ros::Duration(0.3).sleep(); // prevent from saving 0 to start_time
         this->setStartTime();
-        ROS_INFO("start time set!");
         
         ros::Rate lr(10);
 
@@ -45,15 +56,14 @@ public:
                 cmd.linear.x = 0.3;
                 distance = cmd.linear.x * duration_time.toSec();
 
-                // ROS_INFO("distance: %lf", distance);
-                // ROS_INFO("total time: %lf", duration_time.toSec());
+                ROS_INFO("distance: %lf", distance);
+                ROS_INFO("total time: %lf", duration_time.toSec());
                 
                 if(distance > 1.5) {
                     ROS_INFO("robot stopped");
                     this->sequence = 1;
                     distance = 0.0; //reset
                     cmd.linear.x = 0.0;
-                    this->current_time = ros::Time::now();
                     this->setStartTime();
                 }
 
@@ -61,10 +71,11 @@ public:
                 cmd.linear.x = 0.0;
                 cmd.angular.z = 0.3;
                 ROS_INFO("robot rotate");
-                
-                ROS_INFO("total time: %lf", duration_time.toSec());
-                if(duration_time.toSec() > 3.0) {
-                    
+                // ROS_INFO("total time: %lf", duration_time.toSec());
+
+                ROS_INFO("yaw : %lf", this->yaw);
+
+                if(duration_time.toSec() > 4.0) {
                     cmd.angular.z = 0.0;
                     this->sequence = 2;
                     this->setStartTime();
@@ -99,6 +110,35 @@ public:
         }
     }
 
+    void odomCb(const nav_msgs::Odometry::ConstPtr &msg) {
+        this->odom_msg.pose.pose.orientation.x = msg->pose.pose.orientation.x;
+        this->odom_msg.pose.pose.orientation.y = msg->pose.pose.orientation.y;
+        this->odom_msg.pose.pose.orientation.z = msg->pose.pose.orientation.z;
+        this->odom_msg.pose.pose.orientation.w = msg->pose.pose.orientation.w;
+        // this->yaw = this->quaternionToYaw();
+    }
+
+    double quaternionToYaw() {
+        // quartnion to r p y
+        tf::Quaternion q(
+            this->odom_msg.pose.pose.orientation.x,
+            this->odom_msg.pose.pose.orientation.y,
+            this->odom_msg.pose.pose.orientation.z,
+            this->odom_msg.pose.pose.orientation.w
+        );
+
+        tf::Matrix3x3 m(q);
+        double r, p, y;
+        m.getRPY(r, p, y); //yaw's value updated right away
+
+        return y;
+    }
+
+    void scanCb(const sensor_msgs::LaserScan::ConstPtr &msg) {
+        ROS_INFO("Scan %f", msg->ranges[0]);
+        // this->scan_msg.ranges[0] = msg->ranges[0];
+    }
+
 };
 
 
@@ -106,7 +146,6 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "driving_and_picking_up_node");
 
     DriveNPick DriveObj;
-    
 
     ros::waitForShutdown();
     ros::shutdown();
